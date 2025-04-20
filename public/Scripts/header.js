395d76313de88cd2a7379548386f51e0f5d27a4a
Andreea Pop <apop1837@conestogac.on.ca>
2025-04-20 19:29:47 -0400

    Change navigation, add font awesome

--- public/Scripts/header.js
+++ public/Scripts/header.js
@@ -10,32 +10,71 @@ const content = `
     <div class="title">aid's small corner</div>
     <div class="subtext">a little personal site</div>
 </header>
-<nav>
-    <ul>
-        <li><a href="/index.html">home</a></li>
-        <li><a href="/about.html">about me</a></li>
-        <li class="disabled">ocs / shrines</li>
-        <li>
-            fun things
-            <ul>
-                <li><a href="/minecraft.html">minecraft world</a></li>
-                <li class="disabled">recipes</li>
-            </ul>
-        </li>
-        <li><a href="/blogging.html">blog</a></li>
-        <li><a href="https://asuraid.atabook.org/" target="_blank">guestbook</a></li>
-        <li>
-            other links
-            <ul>
-                <li><a href="/manifesto.html">site manifesto</a></li>
-                <li><a href="/links.html">links</a></li>
-                <li><a href="/credits.html">credits</a></li>
-                <li><a href="/sitemap.html">sitemap</a></li>
-            </ul>
-        </li>
-    </ul>
-</nav>
+<div class="topnavbg">
+    <div class="topnav" id="myTopnav">
+        <a href="/index.html" class="active">Home</a>
+        <a href="/about.html">About Me</a>
+        <div class="dropdown">
+            <button class="dropbtn">
+                Personal
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="/blogging.html">Blog</a>
+                <a href="/manifesto.html">Manifesto</a>
+                <a href="/to-do.html">To-do List</a>
+            </div>
+        </div>
+        <div class="dropdown">
+            <button class="dropbtn">
+                OCs / Shrines
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="#" class="disabled">Sylvie</a>
+                <a href="#" class="disabled">Zhongvie (💖)</a>
+                <a href="#" class="disabled">Yadyra</a>
+                <a href="#" class="disabled">Nameless</a>
+            </div>
+        </div>
+        <div class="dropdown">
+            <button class="dropbtn">
+                Fun Things
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="/minecraft.html">Minecraft Forever World</a>
+                <a href="/links.html">Neighbours / Webrings
+                <a href="#" class="disabled">Recipes</a>
+            </div>
+        </div>
+        <div class="dropdown">
+            <button class="dropbtn">
+                Other
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="https://asuraid.atabook.org/">Guestbook</a>
+                <a href="#" class="disabled">Resources</a>
+                <a href="/sitemap.html">Sitemap</a>
+                <a href="/credits.html">Credits</a>
+            </div>
+        </div>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+    </div>
+</div>
+
 `;
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
 class NavHeader extends HTMLElement {
     constructor() {
