f2b62abce658b2fcd72ecf99cc21ee090c27cbca
Andreea Pop <apop1837@conestogac.on.ca>
2025-04-21 14:44:56 -0400

    Trying to figure out keyboard dropdown

--- public/Scripts/dropdown.js
+++ public/Scripts/dropdown.js
@@ -1,9 +1,28 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+var menuItems = document.querySelectorAll("topnav");
+Array.prototype.forEach.call(menuItems, function (el, i) {
+    el.querySelector("button").addEventListener("click", function (event) {
+        if (this.parentNode.className == "dropbtn") {
+            this.parentNode.className = "has-submenu open";
+            this.setAttribute("aria-expanded", "true");
+        } else {
+            this.parentNode.className = "has-submenu";
+            this.setAttribute("aria-expanded", "false");
+        }
+        event.preventDefault();
+        return false;
+    });
+});
+
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+  document.getElementById("myDropdown").classList.toggle("show");
 }
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+};
--- public/Scripts/header.js
+++ public/Scripts/header.js
@@ -19,7 +19,7 @@ const content = `
                 Personal
                 <i class="fa fa-caret-down"></i>
             </button>
-            <div class="dropdown-content">
+            <div class="dropdown-content" id="myDropdown">
                 <a href="/blogging.html">Blog</a>
                 <a href="/manifesto.html">Manifesto</a>
                 <a href="/to-do.html">To-do List</a>
@@ -30,7 +30,7 @@ const content = `
                 OCs / Shrines
                 <i class="fa fa-caret-down"></i>
             </button>
-            <div class="dropdown-content">
+            <div class="dropdown-content" id="myDropdown">
                 <a href="#" class="disabled">Sylvie</a>
                 <a href="#" class="disabled">Zhongvie (💖)</a>
                 <a href="#" class="disabled">Yadyra</a>
@@ -42,7 +42,7 @@ const content = `
                 Fun Things
                 <i class="fa fa-caret-down"></i>
             </button>
-            <div class="dropdown-content">
+            <div class="dropdown-content" id="myDropdown">
                 <a href="/minecraft.html">Minecraft Forever World</a>
                 <a href="/links.html">Neighbours / Webrings
                 <a href="#" class="disabled">Recipes</a>
@@ -53,21 +53,38 @@ const content = `
                 Other
                 <i class="fa fa-caret-down"></i>
             </button>
-            <div class="dropdown-content">
+            <div class="dropdown-content" id="myDropdown">
                 <a href="https://asuraid.atabook.org/">Guestbook</a>
                 <a href="#" class="disabled">Resources</a>
                 <a href="/sitemap.html">Sitemap</a>
                 <a href="/credits.html">Credits</a>
             </div>
         </div>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+        <a href="javascript:void(0);" class="icon" onclick="openDropdown()">&#9776;</a>
     </div>
 </div>
 
 `;
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
 function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+    if (!e.target.matches(".dropbtn")) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains("show")) {
+            myDropdown.classList.remove("show");
+        }
+    }
+};
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/* FOR MOBILE DO NOT TOUCH OTHERWISE */
+function openDropdown() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
