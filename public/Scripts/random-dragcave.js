46d376c2c260375b04c2b26b49756a8a7df916e6
Andreea Pop <apop1837@conestogac.on.ca>
2025-04-29 21:11:26 -0400

    Add randomizer for dragon cave

---
+++ public/Scripts/random-dragcave.js
@@ -0,0 +1,15 @@
+const dragon = ["htdxM", "kxCPF", "6494P", "NoTGK", "vvudZ", "5oTHl", "EI9gO", "4csMR", "fEo2q", "IjZLN"];
+
+// this chooses a random number from all available text indices
+var randomDragon = dragon[Math.floor(Math.random() * dragon.length)];
+console.log(randomDragon);
+// make the URL into a proper image tag
+const image =
+    "<a href='https://dragcave.net/view/" +
+    randomDragon +
+    "'><img src='https://dragcave.net/image/" +
+    randomDragon +
+    ".gif' class='image' style='margin-left: auto; margin-right: auto; padding: 10px; margin-bottom: 0' alt='' aria-hidden='true'/></a>";
+console.log(image);
+// append to the div
+document.getElementById("dragon-cave").innerHTML = image;
