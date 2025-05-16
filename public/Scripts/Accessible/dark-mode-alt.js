cfa62b0ff827023e36cb78d1c9774d11c8fbf42f
Andreea Pop <apop1837@conestogac.on.ca>
2025-05-16 14:14:06 -0400

    Fix variables?

--- public/Scripts/Accessible/dark-mode-alt.js
+++ public/Scripts/Accessible/dark-mode-alt.js
@@ -2,7 +2,7 @@
 let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)");
 // this checks for session storage telling to override
 // the system preferences
-let theme = sessionStorage.getItem("theme");
+let theme = sessionStorage.getItem("site-theme");
 
 if (systemInitiatedDark.matches) {
     document.getElementById("theme-toggle").innerHTML = "☀️";
@@ -15,47 +15,47 @@ function prefersColorTest(systemInitiatedDark) {
         document.documentElement.setAttribute("data-theme", "dark");
         document.getElementById("theme-toggle").innerHTML = "☀️";
         // this clears the session storage
-        sessionStorage.setItem("theme", "");
+        sessionStorage.setItem("site-theme", "");
     } else {
         document.documentElement.setAttribute("data-theme", "light");
         document.getElementById("theme-toggle").innerHTML = "🌙";
-        sessionStorage.setItem("theme", "");
+        sessionStorage.setItem("site-theme", "");
     }
 }
 systemInitiatedDark.addListener(prefersColorTest);
 
 function modeSwitcher() {
     // it’s important to check for overrides again
-    let theme = sessionStorage.getItem("theme");
+    let theme = sessionStorage.getItem("site-theme");
     // checks if reader selected dark mode
     if (theme === "dark") {
         document.documentElement.setAttribute("data-theme", "light");
-        sessionStorage.setItem("theme", "light");
+        sessionStorage.setItem("site-theme", "light");
         document.getElementById("theme-toggle").innerHTML = "🌙";
         // checks if reader selected light mode
     } else if (theme === "light") {
         document.documentElement.setAttribute("data-theme", "dark");
-        sessionStorage.setItem("theme", "dark");
+        sessionStorage.setItem("site-theme", "dark");
         document.getElementById("theme-toggle").innerHTML = "☀️";
         // checks if system set dark mode
     } else if (systemInitiatedDark.matches) {
         document.documentElement.setAttribute("data-theme", "light");
-        sessionStorage.setItem("theme", "light");
+        sessionStorage.setItem("site-theme", "light");
         document.getElementById("theme-toggle").innerHTML = "🌙";
         // the only option left is system set light mode
     } else {
         document.documentElement.setAttribute("data-theme", "dark");
-        sessionStorage.setItem("theme", "dark");
+        sessionStorage.setItem("site-theme", "dark");
         document.getElementById("theme-toggle").innerHTML = "☀️";
     }
 }
 
 if (theme === "dark") {
     document.documentElement.setAttribute("data-theme", "dark");
-    sessionStorage.setItem("theme", "dark");
+    sessionStorage.setItem("site-theme", "dark");
     document.getElementById("theme-toggle").innerHTML = "☀️";
 } else if (theme === "light") {
     document.documentElement.setAttribute("data-theme", "light");
-    sessionStorage.setItem("theme", "light");
+    sessionStorage.setItem("site-theme", "light");
     document.getElementById("theme-toggle").innerHTML = "🌙";
 }
