0773166f72ac06078fef29ffa512db849050620a
Andreea Pop <apop1837@conestogac.on.ca>
2025-05-17 19:35:26 -0400

    Remove console.log

--- public/Scripts/random-dragcave.js
+++ public/Scripts/random-dragcave.js
@@ -2,7 +2,7 @@ const dragon = ["pBOKw", "E8Niv", "2MyJp", "37jPT", "GkntK", "BNWyv", "TEjqe", "
 
 // this chooses a random number from all available text indices
 var randomDragon = dragon[Math.floor(Math.random() * dragon.length)];
-console.log(randomDragon);
+
 // make the URL into a proper image tag
 const image =
     "<a href='https://dragcave.net/view/" +
@@ -10,6 +10,6 @@ const image =
     "'><img src='https://dragcave.net/image/" +
     randomDragon +
     ".gif' class='image' style='margin-left: auto; margin-right: auto; padding: 10px; margin-bottom: 0' alt='' aria-hidden='true'/></a>";
-console.log(image);
+
 // append to the div
 document.getElementById("dragon-cave").innerHTML = image;
