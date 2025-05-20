b9cacbb2f9761bc02591accad96e21a99653fc53
Andreea Pop <apop1837@conestogac.on.ca>
2025-05-20 19:10:34 -0400

    Update

--- public/Scripts/random-dragcave.js
+++ public/Scripts/random-dragcave.js
@@ -1,4 +1,4 @@
-const dragon = ["AHfCU", "E8Niv", "2MyJp", "37jPT", "GkntK", "7PJT1", "TEjqe", "Ewxrk"];
+const dragon = ["AHfCU", "E8Niv", "2MyJp", "GkntK", "7PJT1", "Ewxrk"];
 
 // this chooses a random number from all available text indices
 var randomDragon = dragon[Math.floor(Math.random() * dragon.length)];
