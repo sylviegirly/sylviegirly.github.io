69fdbc9e6e4fb7e97fa95b9e256c85b3b83f71c9
Andreea Pop <apop1837@conestogac.on.ca>
2025-05-27 01:25:39 -0400

    Media log

--- public/Scripts/tabs.js
+++ public/Scripts/tabs.js
@@ -61,6 +61,27 @@ function openBlog(evt, blogName) {
     evt.currentTarget.className += " active";
 }
 
+function openMediaTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="mediaTab" and hide them
+    tabcontent = document.getElementsByClassName("mediaTab");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
 // Get the element with id="defaultOpen" and click on it
 if (document.getElementById("defaultOpen") != null) {
     document.getElementById("defaultOpen").click();
@@ -75,4 +96,10 @@ if (document.getElementById("defaultAltOpen") != null) {
 // Get the element with id="defaultAltOpen" and click on it
 if (document.getElementById("blogActive") != null) {
     document.getElementById("blogActive").click();
+}
+
+// Get the element with id="blogActive" and click on it
+// Get the element with id="defaultAltOpen" and click on it
+if (document.getElementById("mediaActive") != null) {
+    document.getElementById("mediaActive").click();
 }
