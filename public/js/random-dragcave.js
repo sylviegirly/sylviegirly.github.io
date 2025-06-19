d58a577e19ba06a12c9c179128f1e8151ef5ccde
Andreea Pop <apop1837@conestogac.on.ca>
2025-06-19 07:45:01 -0400

    Update

--- public/js/random-dragcave.js
+++ public/js/random-dragcave.js
@@ -1,4 +1,4 @@
-const dragon = ["1F2mP", "9VQBX", "S1VsY", "XXrBA", "3XEgh", "6YnAm", "meQ7x", "lwuKM", "b2kCi", "rE148"];
+const dragon = ["1DR8g", "1F2mP", "6tVs1", "9VQBX", "5siAi", "3XEgh", "6YnAm", "meQ7x", "b2kCi", "rE148"];
 
 // this chooses a random number from all available text indices
 var randomDragon = dragon[Math.floor(Math.random() * dragon.length)];
@@ -9,7 +9,7 @@ const image =
     randomDragon +
     "'><img src='https://dragcave.net/image/" +
     randomDragon +
-    ".gif' class='image' style='margin-left: auto; margin-right: auto; padding: 10px; margin-bottom: 0' alt='' aria-hidden='true'/></a>";
+    ".gif' class='image' style='margin-left: auto; margin-right: auto; padding: 10px; margin-bottom: 0' alt='' aria-hidden='true' loading='lazy'/></a>";
 
 // append to the div
 document.getElementById("dragon-cave").innerHTML = image;
