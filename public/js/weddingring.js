1b7e5b3b267fe63a9bfeb562b3375fee204ddc6b
Andreea Pop <apop1837@conestogac.on.ca>
2025-07-27 17:47:12 -0400

    Update

--- public/js/weddingring.js
+++ public/js/weddingring.js
@@ -1,4 +1,4 @@
-document.querySelector("#lover").src = "https://file.garden/Zq0xOX6Iv3i3b1te/Neocities/Other/ZLedit3_thumb.png";
+document.querySelector("#lover").src = "https://file.garden/Zq0xOX6Iv3i3b1te/Neocities/Other/ZLedit4_thumb.png";
 document.querySelector("#yumewm").textContent = "beefy tree god";
 document.querySelector("#yumeaction").textContent = "kissing";
 document.querySelector("#yumeship").textContent = "zhongli!";
