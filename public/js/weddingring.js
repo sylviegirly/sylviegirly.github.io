752d2a94ce2df833c98395af43f20c90382bdb14
Andreea Pop <apop1837@conestogac.on.ca>
2025-07-27 17:20:56 -0400

    Index working

---
+++ public/js/weddingring.js
@@ -0,0 +1,4 @@
+document.getElementbyId("lover").src = "https://file.garden/Zq0xOX6Iv3i3b1te/Neocities/Other/ZLedit3_thumb.png";
+document.getElementbyId("yumewm").textContent = "tree god";
+document.getElementById("yumeaction").textContent = "kissing";
+document.getElementById("yumeship").textContent = "zhongli!";
