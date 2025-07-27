3289837da07e99c227aa47f2551d75af945091ca
Andreea Pop <apop1837@conestogac.on.ca>
2025-07-27 17:45:49 -0400

    Fixing webring

--- public/js/weddingring.js
+++ public/js/weddingring.js
@@ -1,4 +1,4 @@
-document.getElementbyId("lover").src = "https://file.garden/Zq0xOX6Iv3i3b1te/Neocities/Other/ZLedit3_thumb.png";
-document.getElementbyId("yumewm").textContent = "tree god";
-document.getElementById("yumeaction").textContent = "kissing";
-document.getElementById("yumeship").textContent = "zhongli!";
+document.querySelector("#lover").src = "https://file.garden/Zq0xOX6Iv3i3b1te/Neocities/Other/ZLedit3_thumb.png";
+document.querySelector("#yumewm").textContent = "beefy tree god";
+document.querySelector("#yumeaction").textContent = "kissing";
+document.querySelector("#yumeship").textContent = "zhongli!";
