073815380985089487a8e061ff6a7082f5182ee4
Andreea B. Pop <andreeabiancapop@tutanota.com>
2025-08-23 00:40:20 -0400

    WIP about me update

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -19,27 +19,6 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
-function openAltTab(evt, tabName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabAltcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tabAltlinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-
 function openBlog(evt, blogName) {
     // Declare all variables
     var i, blogcontent, bloglinks;
