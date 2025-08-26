6d25b0fbb1c7e0f71253efe39fbc2df9a387b8c2
Andreea B. Pop <andreeabiancapop@tutanota.com>
2025-08-26 12:46:42 -0400

    Archive minecraft, update blog

--- public/js/header.js
+++ public/js/header.js
@@ -116,9 +116,8 @@ const content = `
             <a id="link-5-0-0" href="#">Fun Things</a>
             <ul id="menu-5">
                 <li id="item-5-1-0"><a id="link-5-1-0" href="/media-log.html">Media Log</a></li>
-                <li id="item-5-2-0"><a id="link-5-2-0" href="/minecraft.html">Minecraft Forever World</a></li>
-                <li id="item-5-3-0"><a id="link-5-3-0" href="/recipes.html">Recipes</a></li>
-                <li id="item-5-4-0"><a id="link-5-4-0" href="/toybox.html">Toybox</a></li>
+                <li id="item-5-2-0"><a id="link-5-2-0" href="/recipes.html">Recipes</a></li>
+                <li id="item-5-3-0"><a id="link-5-3-0" href="/toybox.html">Toybox</a></li>
             </ul>
         </li>
         <li id="item-6-0-0" class="dropdown">
