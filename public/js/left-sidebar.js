7da32da134bb02dca67d89f214fd76bf8b6a70a4
Andreea B. Pop <andreeabiancapop@tutanota.com>
2025-09-21 00:46:32 -0400

    Fix button

--- public/js/left-sidebar.js
+++ public/js/left-sidebar.js
@@ -98,11 +98,11 @@ const sidebar = `
         <div class="site-button">
             <div class="images-row">
                 <a href="https://zhongvie.neocities.org/" target="_blank"
-                    ><img src="https://file.garden/Zq0xOX6Iv3i3b1te/Neocities/sylvieButton.gif" alt="zhongvie button"
+                    ><img src="./img/sylvieButton.gif" alt="zhongvie button"
                 /></a>
             </div>
             <textarea id="site-button" name="site-button">
-&lt;a href="https://zhongvie.neocities.org/"&gt;&lt;img src="./img/sylvieButton.gif" alt="zhongvie button" loading="lazy"&gt;&lt;/a&gt;</textarea
+&lt;a href="https://zhongvie.neocities.org/"&gt;&lt;img src="https://file.garden/Zq0xOX6Iv3i3b1te/Neocities/sylvieButton.gif" alt="zhongvie button" loading="lazy"&gt;&lt;/a&gt;</textarea
             >
             <label for="site-button" class="subtle" style="text-align: center; margin-top: 3px;"
                 >Site button; hotlinking is ok since the URL is on filegarden, but it's recommended to save. Alt buttons can be found <a href="./links.html">here</a></label
