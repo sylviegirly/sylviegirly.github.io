ed65487975b65a5e2166e93fae589a06b1b7776f
Andreea B. Pop <andreeabiancapop@tutanota.com>
2025-09-26 01:19:02 -0400

    Hard code others

--- public/oc/character-nav.js
+++ public/oc/character-nav.js
@@ -94,8 +94,8 @@ const content = `
             <a id="link-4-0-0" href="#">OCs / Shrines</a>
             <ul id="menu-4">
                 <li id="item-4-1-0"><a id="link-4-1-0" href="/oc/sylvie/index.html">Sylvie</a></li>
-                <li id="item-4-2-0"><a id="link-4-2-0" href="yadyra.html">Yadyra</a></li>
-                <li id="item-4-3-0"><a id="link-4-3-0" href="nameless.html">Nameless</a></li>
+                <li id="item-4-2-0"><a id="link-4-2-0" href="/oc/yadyra.html">Yadyra</a></li>
+                <li id="item-4-3-0"><a id="link-4-3-0" href="/oc/nameless.html">Nameless</a></li>
                 <li id="item-4-4-0"><a id="link-4-4-0" href="/oc.html">Other OCs</a></li>
                 <li id="item-4-5-0" class="dropdown">
                     <a id="link-4-5-0" href="#">Shrines</a>
