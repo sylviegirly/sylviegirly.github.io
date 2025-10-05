c84d8a075c08e47b7476d3fb70dcf593c6e562f6
Andreea B. Pop <andreeabiancapop@tutanota.com>
2025-10-05 02:03:42 -0400

    Remove redundant JS

--- public/js/tab-title-suffix.js
+++ public/js/tab-title-suffix.js
@@ -1,4 +1,4 @@
 const tabTitle = document.title;
 if (tabTitle !== "aid's little corner") {
-	document.title = tabTitle + " | aid's little corner";
+	document.title = tabTitle + " • aid's little corner";
 }
