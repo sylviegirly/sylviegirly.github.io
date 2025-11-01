6dc954f9257949da93e79e8d93fca91d438da684
Andreea B. Pop <andreeabiancapop@tutanota.com>
2025-10-31 22:50:27 -0400

    Fix back to normal colours

--- public/js/left-sidebar.js
+++ public/js/left-sidebar.js
@@ -43,7 +43,7 @@ const sidebar = `
     <section>
         <div class="img-floatinganim">
             <img class="dendro"
-                src="./img/halloweenie/season-hall-pumpkin-orange.png" aria-hidden='true' alt='' />
+                src="/img/dendro_symbol.png" aria-hidden='true' alt='' />
         </div>
         <div id="img-load"></div>
         <h4>
