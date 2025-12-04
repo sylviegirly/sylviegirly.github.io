51a7b3262a739ab07b22aa78838d3dd71322084a
Andreea B. Pop <andreeabiancapop@tutanota.com>
2025-12-04 15:49:52 -0500

    Add snowflakes

--- public/js/layout.js
+++ public/js/layout.js
@@ -40,6 +40,36 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+/* SNOWFLAKES, will remove not during december */
+function createSnowflake() {
+    const snowflake = document.createElement("div");
+    snowflake.classList.add("snowflake");
+    snowflake.textContent = "•";
+    snowflake.style.fontSize = Math.random() * 24 + 10 + "px";
+    snowflake.style.left = Math.random() * window.innerWidth + "px";
+    snowflake.style.animation = `fall ${Math.random() * 4 + 4}s linear infinite, sideWays ${Math.random() * 2 + 1}s ease-in-out infinite`;
+
+    document.body.appendChild(snowflake);
+
+    setTimeout(
+        () => {
+            snowflake.remove();
+        },
+        Math.random() * 4000 + 4000
+    );
+}
+
+document.styleSheets[0].insertRule(
+    "@keyframes fall { 0% { top: -50px; } 100% { top: 100vh; } }",
+    document.styleSheets[0].cssRules.length
+);
+document.styleSheets[0].insertRule(
+    "@keyframes sideWays { 0%, 100% { transform: translateX(0); } 50% { transform: translateX(20px); } }",
+    document.styleSheets[0].cssRules.length
+);
+
+setInterval(createSnowflake, 200);
+
 /* FREEZE GIFS */
 // Load user preference from localStorage (on page load or after toggle)
 function loadUserPreference() {
