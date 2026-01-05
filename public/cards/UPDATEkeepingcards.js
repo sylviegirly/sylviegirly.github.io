ba40f9f65fc6250076b51da70c12b18bcb8e5d78
Andreea B. Pop <andreeabiancapop@tutanota.com>
2026-01-05 14:19:00 -0500

    Update collection

--- public/cards/UPDATEkeepingcards.js
+++ public/cards/UPDATEkeepingcards.js
@@ -5,4 +5,4 @@
     ex: "card01, card02...."
 */
 var keepingcards =
-    "qucusaurus01, dagobah01, truepassion11, laprimera01, triplecone01, rust01, scepter01, abundance12, cortondo02, firstlie01, vanarana15, vasavi12, nirvasyl01, burgers01, illness01, psychology01, lapisdei01, cervanivea01, warmonk01, lapisdei07, lapisdei10, jadedeer06, objection15";
+    "vanarana15, vasavi12, burgers01, illness01, psychology01, lapisdei01, cervanivea01, warmonk01, lapisdei07, lapisdei10, jadedeer06, objection15";
