e892f6edd0cd8217e23e983f5c3643d7ac1f0772
Aid 🌿 <51125909+sylviegirly@users.noreply.github.com>
2026-01-07 02:40:26 -0500

    Update UPDATEkeepingcards.js

--- public/cards/js/UPDATEkeepingcards.js
+++ public/cards/js/UPDATEkeepingcards.js
@@ -5,4 +5,4 @@
     ex: "card01, card02...."
 */
 var keepingcards =
-    "cervanivea15, warmonk12, lapisdei03, lapisdei04, lapisdei05, lapisdei06, illness06, jadedeer04, vasavi07, objection17, warmonk06, burgers02, objection01, psychology03, psychology02, jadedeer01, illness02, lapisdei20, vasavi13, vanarana15, vasavi12, burgers01, illness01, psychology01, lapisdei01, cervanivea01, warmonk01, lapisdei07, lapisdei10, jadedeer06, objection15";
+    "cervanivea17, cervanivea15, warmonk12, lapisdei03, lapisdei04, lapisdei05, lapisdei06, illness06, jadedeer04, vasavi07, objection17, warmonk06, burgers02, objection01, psychology03, psychology02, jadedeer01, illness02, lapisdei20, vasavi13, vanarana15, vasavi12, burgers01, illness01, psychology01, lapisdei01, cervanivea01, warmonk01, lapisdei07, lapisdei10, jadedeer06, objection15";
