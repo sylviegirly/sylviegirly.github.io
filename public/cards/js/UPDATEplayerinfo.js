afdbd414934c854fe024258b9cd8be355782e595
Andreea B. Pop <andreeabiancapop@tutanota.com>
2026-01-08 22:48:53 -0500

    Update

--- public/cards/js/UPDATEplayerinfo.js
+++ public/cards/js/UPDATEplayerinfo.js
@@ -5,7 +5,7 @@
 
     ex: ["PLAYERNAME1: card01, card02", "PLAYERNAME2: card03, card04"]
 */
-var pendingtrades = ["netbug009: sig_aid"];
+var pendingtrades = ["netbug009: sig_aid", "Silly Blanks 301: lawless19, speedrunner06"];
 
 /*
    HIGH PRIORITY DECKS:
