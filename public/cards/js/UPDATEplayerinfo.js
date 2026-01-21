df5b0aac3d9fdd45a2da3f3bc61561ba56a69749
Andreea B. Pop <andreeabiancapop@tutanota.com>
2026-01-21 01:23:12 -0500

    Remove pending

--- public/cards/js/UPDATEplayerinfo.js
+++ public/cards/js/UPDATEplayerinfo.js
@@ -5,11 +5,7 @@
 
     ex: ["PLAYERNAME1: card01, card02", "PLAYERNAME2: card03, card04"]
 */
-var pendingtrades = [
-   "netbug009: sig_aid",
-   "SillyBlanks302: driving03, worst05",
-   "SwitchItUp284: disgrace04, disgrace06"
-];
+var pendingtrades = ["netbug009: sig_aid", "SillyBlanks302: driving03, worst05"];
 
 /*
    HIGH PRIORITY DECKS:
