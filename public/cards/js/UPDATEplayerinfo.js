12034a3af4e8ab04d819a25882c30985adfb28fc
Andreea B. Pop <andreeabiancapop@tutanota.com>
2026-01-30 14:13:17 -0500

    Add new silly blanks

--- public/cards/js/UPDATEplayerinfo.js
+++ public/cards/js/UPDATEplayerinfo.js
@@ -8,7 +8,8 @@
 var pendingtrades = [
    "netbug009: sig_aid",
    "SillyBlanks303: greatest15, pay08",
-   "camilovesyou: catcafe16, raidraptors12, swimmer15, sig_aid"
+   "camilovesyou: catcafe16, raidraptors12, swimmer15, sig_aid",
+   "SillyBlanks304: severe10, mothers09"
 ];
 
 /*
