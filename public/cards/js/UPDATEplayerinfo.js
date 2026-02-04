90a969c4ca2e710bf96f88ec8253e46cc9e4fbd5
Andreea B. Pop <andreeabiancapop@tutanota.com>
2026-02-04 14:12:31 -0500

    Update

--- public/cards/js/UPDATEplayerinfo.js
+++ public/cards/js/UPDATEplayerinfo.js
@@ -32,9 +32,9 @@ var pendingtrades = [
    ex: "deckone, decktwo, deckthree" or "card01, card02, card03"
 */
 var highprioritydecks =
-   "lostlight, hyposelenia, blacksalt, illness, vasavi, vanarana, burgers, psychology, warmonk, objection, pal, coffee, cuarta, townhall, roost, fragment, dragonscale"; // "deckone, decktwo, deckthree..."
+   "hyposelenia, blacksalt, illness, vasavi, vanarana, burgers, psychology, warmonk, objection, pal, coffee, cuarta, townhall, roost, fragment, dragonscale"; // "deckone, decktwo, deckthree..."
 var allisodecks =
-   "goldweaver, emergency, zangetsu, squeak, songbird, selfless, animula, fleshlings, husbandos, autobots, decepticons, pal, coffee, townhall, roost, fragment, dragonscale, e-ming, ruoye, logic, objection, illness, nirvasyl, omniscient, vasavi, warmonk, psychology, burgers, vanarana, cuarta, destron, convoy, vector, warmaiden, pillowfort, sapientia, holypriest, spectrum, matoi, burnish, mysterious, hyposelenia, eternaleye, blacksalt, lostlight, ashcat, easilylost, awoo, nasod"; // "deckone, decktwo, deckthree..."
+   "goldweaver, emergency, zangetsu, squeak, songbird, selfless, animula, fleshlings, husbandos, autobots, decepticons, pal, coffee, townhall, roost, fragment, dragonscale, e-ming, ruoye, logic, objection, illness, nirvasyl, omniscient, vasavi, warmonk, psychology, burgers, vanarana, cuarta, destron, convoy, vector, warmaiden, pillowfort, sapientia, holypriest, spectrum, matoi, burnish, mysterious, hyposelenia, eternaleye, blacksalt, ashcat, easilylost, awoo, nasod"; // "deckone, decktwo, deckthree..."
 var singlecards = "yaoi02"; // "card01, card02, card03..."
 
 /*
