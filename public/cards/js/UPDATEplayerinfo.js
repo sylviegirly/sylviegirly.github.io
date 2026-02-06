873c4bb97a0aa80ff8c906c8aeb3779c1ed05512
Andreea B. Pop <andreeabiancapop@tutanota.com>
2026-02-06 16:12:34 -0500

    Update

--- public/cards/js/UPDATEplayerinfo.js
+++ public/cards/js/UPDATEplayerinfo.js
@@ -9,7 +9,8 @@ var pendingtrades = [
    "netbug009: sig_aid",
    "camilovesyou: raidraptors12, swimmer15, sig_aid",
    "SillyBlanks305: onion13, pose04",
-   "moes: expectations18, glimmerlab04, kyun-kyun12, morikubos02, secretaries20, usurped12, ariatrading12, babalon08, beast07, birds10"
+   "moes: expectations18, glimmerlab04, kyun-kyun12, morikubos02, secretaries20, usurped12, ariatrading12, babalon08, beast07, birds10",
+   "errorysm: croceamors15, value19, jello18, sig_aid"
 ];
 
 /*
