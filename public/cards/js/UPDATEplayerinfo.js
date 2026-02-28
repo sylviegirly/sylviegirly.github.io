0a9bdf169b7a036af0d13696688df715e0dc98e3
Andreea B. Pop <andreeabiancapop@tutanota.com>
2026-02-28 17:28:15 -0500

    Remove psychology

--- public/cards/js/UPDATEplayerinfo.js
+++ public/cards/js/UPDATEplayerinfo.js
@@ -27,7 +27,7 @@ var pendingtrades = ["swapstation267: eternaleye01, eternaleye01"];
    ex: "deckone, decktwo, deckthree" or "card01, card02, card03"
 */
 var highprioritydecks =
-   "animula, blacksalt, chiliarch, coffee, cuarta, dragonscale, dreadlord, equaalba, eun, fragment, holypriest, hyposelenia, nasod, omniscient, pal, psychology, roost, squeak, townhall, truename, vanarana"; // "deckone, decktwo, deckthree..."
+   "animula, blacksalt, chiliarch, coffee, cuarta, dragonscale, dreadlord, equaalba, eun, fragment, holypriest, hyposelenia, nasod, omniscient, pal, roost, squeak, townhall, truename, vanarana"; // "deckone, decktwo, deckthree..."
 var allisodecks =
    "waifei, silvertree, restored, truename, chiliarch, dreadlord, equaalba, flowery, educate, wildkat, primera, thewary, conqueror, viatrix, tablesalt, tercera, paradisaea, tower, easilylost, eun, crowknights, goldweaver, squeak, selfless, animula, fleshlings, husbandos, autobots, decepticons, pal, coffee, townhall, roost, fragment, dragonscale, e-ming, ruoye, logic, nirvasyl, omniscient, psychology, vanarana, cuarta, destron, convoy, vector, warmaiden, pillowfort, sapientia, holypriest, spectrum, matoi, burnish, mysterious, hyposelenia, eternaleye, blacksalt, ashcat, easilylost, awoo, nasod"; // "deckone, decktwo, deckthree..."
 var singlecards = "yaoi02"; // "card01, card02, card03..."
