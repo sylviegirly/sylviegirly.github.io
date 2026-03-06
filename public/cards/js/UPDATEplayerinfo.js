3e26f0f29b07e1793cb9cf55c0b0acde615e2106
Andreea B. Pop <andreeabiancapop@tutanota.com>
2026-03-05 23:21:21 -0500

    New cards

--- public/cards/js/UPDATEplayerinfo.js
+++ public/cards/js/UPDATEplayerinfo.js
@@ -29,7 +29,7 @@ var pendingtrades = [];
 var highprioritydecks =
    "animula, blacksalt, chiliarch, coffee, dragonscale, dreadlord, equaalba, eun, fragment, holypriest, nasod, omniscient, pal, roost, squeak, townhall, truename, vanarana"; // "deckone, decktwo, deckthree..."
 var allisodecks =
-   "deliverer, nightfury, dotharl, waifei, silvertree, restored, truename, chiliarch, dreadlord, equaalba, flowery, educate, wildkat, primera, thewary, conqueror, viatrix, tablesalt, tercera, paradisaea, tower, easilylost, eun, crowknights, goldweaver, squeak, selfless, animula, fleshlings, husbandos, autobots, decepticons, pal, coffee, townhall, roost, fragment, dragonscale, e-ming, ruoye, logic, nirvasyl, omniscient, vanarana, cuarta, destron, convoy, vector, warmaiden, pillowfort, sapientia, holypriest, spectrum, matoi, burnish, mysterious, eternaleye, blacksalt, ashcat, easilylost, awoo, nasod"; // "deckone, decktwo, deckthree..."
+   "benishidare, deliverer, nightfury, dotharl, waifei, silvertree, restored, truename, chiliarch, dreadlord, equaalba, flowery, educate, wildkat, primera, thewary, conqueror, viatrix, tablesalt, tercera, paradisaea, tower, easilylost, eun, crowknights, goldweaver, squeak, selfless, animula, fleshlings, husbandos, autobots, decepticons, pal, coffee, townhall, roost, fragment, dragonscale, e-ming, ruoye, logic, nirvasyl, omniscient, vanarana, cuarta, destron, convoy, vector, warmaiden, pillowfort, sapientia, holypriest, spectrum, matoi, burnish, mysterious, eternaleye, blacksalt, ashcat, easilylost, awoo, nasod"; // "deckone, decktwo, deckthree..."
 var singlecards = ""; // "card01, card02, card03..."
 
 /*
