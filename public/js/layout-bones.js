01bbdac4b3a7bb8bbc1b5d36f564daf8bb92e37a
A. B. Pop <andreeabiancapop@tutanota.com>
2026-03-13 00:37:59 -0400

    Archive some old pages, add Ecaden

--- public/js/layout-bones.js
+++ public/js/layout-bones.js
@@ -39,8 +39,7 @@ const headerContent = `
                 <li id="item-4-1-0"><a id="link-4-1-0" href="https://zhongvie.bearblog.dev/">Blog</a></li>
                 <li id="item-4-2-0"><a id="link-4-2-0" href="/diary/index.html">Diary</a></li>
                 <li id="item-4-3-0"><a id="link-4-3-0" href="/manifesto.html">Manifesto</a></li>
-                <li id="item-4-4-0"><a id="link-4-4-0" href="/microblog.html">Microblog</a></li>
-                <li id="item-4-5-0"><a id="link-4-5-0" href="/now.html">Now</a></li>
+                <li id="item-4-4-0"><a id="link-4-4-0" href="/now.html">Now</a></li>
             </ul>
         </li>
         <li id="item-5-0-0" class="dropdown">
@@ -60,9 +59,7 @@ const headerContent = `
                 <li id="item-6-2-0"><a id="link-6-2-0" href="/bookmarks.html">Bookmarks</a></li>
                 <li id="item-6-3-0"><a id="link-6-3-0" href="/cards/index.html">Colors TCG</a></li>
                 <li id="item-6-4-0"><a id="link-6-4-0" href="/drawbox.html">Drawbox</a></li>
-                <li id="item-6-5-0"><a id="link-6-5-0" href="/media-log.html">Media Log</a></li>
-                <li id="item-6-6-0"><a id="link-6-6-0" href="/recipes/index.html">Recipes</a></li>
-                <li id="item-6-7-0"><a id="link-6-7-0" href="/templates/index.html">Templates</a></li>
+                <li id="item-6-5-0"><a id="link-6-5-0" href="/templates/index.html">Templates</a></li>
             </ul>
         </li>
         <li id="item-7-0-0" class="dropdown">
